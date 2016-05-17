2118dd21c5b6488b46621ac4c88ff1aaa39eb9f3
lucianopf <lucianopf@outlook.com>
2016-05-17 02:08:40 -0300

    Cansando..

--- app/js/controllers/LoginController.js
+++ app/js/controllers/LoginController.js
@@ -1,11 +1,16 @@
 foodMeApp.controller('LoginController',
-    function LoginController($scope, $location, $q) {
+    function LoginController($scope, $location, $q, $http) {
 
     $scope.username = "";
     $scope.password = "";
+    $scope.email = "";
+    $scope.phone = "";
     $scope.logged = false;
     $scope.path = $location.$$path;
     $scope.session = null;
+    $scope.editUser = false;
+    $scope.adress = "";
+    $scope.teachers = [];
 
     Parse.initialize("app_password");
     Parse.serverURL = 'https://services-users.herokuapp.com/parse'
@@ -105,6 +110,133 @@ foodMeApp.controller('LoginController',
     $scope.togetherShared = function(){
       $scope.togetherInit('Room-1');
     };
+
+    $scope.editUserInfo = function(){
+      $scope.editUser = !$scope.editUser;
+      $scope.username = $scope.session.username;
+      $scope.phone = $scope.session.phone;
+      $scope.email = $scope.session.email;
+      $scope.lat = $scope.session.geo_lat;
+      $scope.long = $scope.session.geo_long;
+      reGeocode($scope.lat,$scope.long)
+        .then(function(res){
+          $scope.adress = res[0].place_name;
+        })
+
+    };
+
+    $scope.cancelEdit = function(){
+      $scope.email = "";
+      $scope.phone = "";
+      $scope.adress = "";
+      $scope.editUser = false;
+      $scope.adress = "";
+    };
+
+    $scope.updateUser = function(){
+      var User = Parse.Object.extend("User");
+      var users = new Parse.Query(User);
+      users.get($scope.session.objectId, {
+        success: function(user) {
+          user.set("email", $scope.email);
+          user.set("phone", $scope.phone);
+          user.set("username", $scope.username);
+          findGeocode($scope.adress)
+            .then(function(res){
+              user.set("geo_lat", res[0].center[0]);
+              user.set("geo_long", res[0].center[1]);
+              user.save(null, {
+                success: function(user) {
+                    $scope.session.email = $scope.email;
+                    $scope.session.phone = $scope.phone;
+                    $scope.session.username = $scope.username;
+                    $scope.editUser = !$scope.editUser;
+                    $scope.$digest();
+                }
+              });
+            })
+        },
+        error: function(object, error) {
+          alert(object + error);
+        }
+      });
+    };
+
+    $scope.initMap = function(){
+      // mapboxgl.accessToken = 'pk.eyJ1IjoibHVjaWFub3BmIiwiYSI6ImEyNTFmYTRkNTA5NjFkZWU3Njk5MDczNDNkMGYxODM1In0.InSeGw4XTNjHQx_bnkXalQ';
+      // var map = new mapboxgl.Map({
+      //     container: 'map', // container id
+      //     style: 'mapbox://styles/lucianopf/cioas0pbg004nagm89waja6sy', //stylesheet location
+      //     center: [-40.3042, -20.2934], // starting position
+      //     zoom: 13 // starting zoom
+      // });
+      L.mapbox.accessToken = 'pk.eyJ1IjoibHVjaWFub3BmIiwiYSI6ImEyNTFmYTRkNTA5NjFkZWU3Njk5MDczNDNkMGYxODM1In0.InSeGw4XTNjHQx_bnkXalQ';
+      var map = L.mapbox.map('map', 'mapbox.streets')
+          .setView([-20.288335, -40.289979], 14);
+
+      var User = Parse.Object.extend("User");
+      var users = new Parse.Query(User);
+      users.equalTo("teacher", true);
+      users.find({
+        success: function(results) {
+          $scope.teachers = results;
+          console.log($scope.teachers);
+          $scope.$apply();
+          results.forEach(function(result){
+            L.mapbox.featureLayer({
+                type: 'Feature',
+                geometry: {
+                    type: 'Point',
+                    coordinates: [
+                      result.attributes.geo_lat,
+                      result.attributes.geo_long
+                    ]
+                },
+                properties: {
+                    title: result.attributes.username,
+                    description: result.attributes.email,
+                    'marker-size': 'large',
+                    'marker-color': '#f55f55',
+                    'marker-symbol': 'star'
+                }
+            }).addTo(map)
+          })
+        },
+        error: function(error) {
+          alert("Error: " + error.code + " " + error.message);
+        }
+      });
+    };
+
+
+    if($scope.path == "/explore"){
+      $scope.initMap();
+    }
+
+    function findGeocode(endereco){
+      var geoFindString = "";
+      geoFindString += "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+      geoFindString += endereco;
+      geoFindString += ".json?access_token=";
+      geoFindString += "pk.eyJ1IjoibHVjaWFub3BmIiwiYSI6ImEyNTFmYTRkNTA5NjFkZWU3Njk5MDczNDNkMGYxODM1In0.InSeGw4XTNjHQx_bnkXalQ";
+      return $http.get(geoFindString)
+      .then(function(response) {
+          return response.data.features;
+      });
+    };
+
+    function reGeocode(lat, long){
+      var geoFindString = "";
+      geoFindString += "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+      geoFindString += lat+","+long;
+      geoFindString += ".json?access_token=";
+      geoFindString += "pk.eyJ1IjoibHVjaWFub3BmIiwiYSI6ImEyNTFmYTRkNTA5NjFkZWU3Njk5MDczNDNkMGYxODM1In0.InSeGw4XTNjHQx_bnkXalQ";
+      return $http.get(geoFindString)
+      .then(function(response) {
+          return response.data.features;
+      });
+    };
+
 
 
 });
--- app/js/controllers/SignupController.js
+++ app/js/controllers/SignupController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 foodMeApp.controller('SignupController',
-    function SignupController($scope, $location, $rootScope) {
+    function SignupController($scope, $location, $rootScope, $http) {
       $scope.username = "";
       $scope.email = "";
       $scope.telephone = "";
@@ -9,6 +9,10 @@ foodMeApp.controller('SignupController',
       $scope.re_password = "";
       $scope.logged = false;
       $scope.teacher = false;
+      $scope.lang_english = false;
+      $scope.lang_spanish = false;
+      $scope.lang_french = false;
+      $scope.lang_deutch = false;
 
       Parse.initialize("app_password");
       Parse.serverURL = 'https://services-users.herokuapp.com/parse';
@@ -20,6 +24,18 @@ foodMeApp.controller('SignupController',
         $location.path('/');
       }
 
+      function findGeocode(endereco){
+        var geoFindString = "";
+        geoFindString += "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+        geoFindString += endereco;
+        geoFindString += ".json?access_token=";
+        geoFindString += "pk.eyJ1IjoibHVjaWFub3BmIiwiYSI6ImEyNTFmYTRkNTA5NjFkZWU3Njk5MDczNDNkMGYxODM1In0.InSeGw4XTNjHQx_bnkXalQ";
+        return $http.get(geoFindString)
+        .then(function(response) {
+            return response.data.features;
+        });
+      };
+
       $scope.clean = function(){
         $scope.username = "";
         $scope.email = "";
@@ -27,9 +43,24 @@ foodMeApp.controller('SignupController',
         $scope.password = "";
         $scope.re_password = "";
         $scope.logged = false;
+        $scope.teacher = false;
+        $scope.lang_english = false;
+        $scope.lang_spanish = false;
+        $scope.lang_french = false;
+        $scope.lang_deutch = false;
       }
 
       $scope.signup = function(){
+        var languages = [];
+        $scope.lang_english ? languages.push("Ingles") : null;
+        $scope.lang_spanish ? languages.push("Espanhol") : null;
+        $scope.lang_french ? languages.push("Françes") : null;
+        $scope.lang_deutch ? languages.push("Alemão") : null;
+        findGeocode($scope.adress)
+          .then(function(res){
+            var lat = res[0].center[0];
+            var long = res[0].center[1];
+          })
         localStorage.removeItem('Parse/app_password/currentUser');
         var user = new Parse.User();
         user.set("username", $scope.username);
@@ -37,17 +68,23 @@ foodMeApp.controller('SignupController',
         user.set("email", $scope.email);
         user.set("teacher", $scope.teacher);
         user.set("phone", $scope.telephone);
+        user.set("languages", languages);
+        findGeocode($scope.adress)
+          .then(function(res){
+            user.set("geo_lat", res[0].center[0]);
+            user.set("geo_long", res[0].center[1]);
+            return user.signUp(null, {
+              success: function(user) {
+                console.log(user);
+                $scope.home();
+              },
+              error: function(user, error) {
+                alert("Erro: " + error.message);
+                $scope.clean();
+              }
+            });
+          })
 
-        user.signUp(null, {
-          success: function(user) {
-            console.log(user);
-            $scope.home();
-          },
-          error: function(user, error) {
-            alert("Erro: " + error.message);
-            $scope.clean();
-          }
-        });
       };
 
       $scope.home = function(){
