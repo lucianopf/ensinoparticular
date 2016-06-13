24c351fe1ea736c34d0ad8d647b8a4c421848072
System Administrator <root@lucianos-MacBook-Air.local>
2016-06-13 01:16:52 -0300

    adding a drawboard

--- app/js/controllers/ClassroomController.js
+++ app/js/controllers/ClassroomController.js
@@ -27,43 +27,63 @@ foodMeApp.controller('ClassroomController',
       $scope.session = session;
     }
 
-    var webrtc = new SimpleWebRTC({
-      // the id/element dom element that will hold "our" video
-      localVideoEl: 'localVideo',
-      // the id/element dom element that will hold remote videos
-      remoteVideosEl: '',
-      // immediately ask for camera access
-      autoRequestMedia: true
+    $scope.videoOn = false;
+
+    $scope.initVideo = function(){
+      $scope.videoOn = true;
+      var webrtc = new SimpleWebRTC({
+        localVideoEl: 'localVideo',
+        remoteVideosEl: '',
+        autoRequestMedia: true
+      });
+      webrtc.on('readyToCall', function () {
+        webrtc.joinRoom('your awesome room name');
+      });
+      $scope.remotesConnected = 0;
+      webrtc.on('videoAdded', function (video, peer) {
+          console.log('video added', peer);
+          var remotes = document.getElementById('remotesVideos');
+          if (remotes) {
+              var container = document.createElement('div');
+              container.className = 'videoContainer';
+              container.id = 'container_' + webrtc.getDomId(peer);
+              container.appendChild(video);
+              video.oncontextmenu = function () { return false; };
+              remotes.appendChild(container);
+              $('.videoContainer').find('video').addClass('12u');
+              $('.videoContainer').find('video').attr('controls',true)
+          }
+      });
+    }
+
+    $scope.togetherOn = true;
+
+    $scope.initTogether = function(){
+      // Config
+      $scope.togetherOn = true;
+      TogetherJS.config("siteName", "EnsinoParticular");
+      TogetherJS.config("toolName", "Compartilhamento");
+      TogetherJS.config("cloneClicks", true);
+      TogetherJS.config("findRoom", "ensinoparticular-hash");
+      TogetherJS.config("suppressJoinConfirmation", true);
+      TogetherJS.config("suppressInvite", true);
+      TogetherJS.config("youtube", true);
+      TogetherJS.config("getUserName", function(){
+        return $scope.username;
+      });
+
+      TogetherJS(this);
+      return false;
+    }
+
+    $(function() {
+      $.each(['#f00', '#ff0', '#0f0', '#0ff', '#00f', '#f0f', '#000', '#fff'], function() {
+        $('#colors_demo .tools').append("<a href='#colors_sketch' class='button small' data-color='" + this + "' style='width: 10px; background: " + this + ";'><span style='visibility: hidden'>.</span></a> ");
+      });
+      $.each([3, 5, 10, 15], function() {
+        $('#colors_demo .tools').append("<a href='#colors_sketch' class='button small' data-size='" + this + "' style='background: #ccc'>" + this + "</a> ");
+      });
+      $('#colors_sketch').sketch();
     });
 
-    // we have to wait until it's ready
-    webrtc.on('readyToCall', function () {
-      // you can name it anything
-      webrtc.joinRoom('your awesome room name');
-    });
-
-    $scope.remotesConnected = 0;
-
-    webrtc.on('videoAdded', function (video, peer) {
-        console.log('video added', peer);
-        var remotes = document.getElementById('remotesVideos');
-        if (remotes) {
-            var container = document.createElement('div');
-            container.className = 'videoContainer';
-            container.id = 'container_' + webrtc.getDomId(peer);
-            container.appendChild(video);
-
-            // suppress contextmenu
-            video.oncontextmenu = function () { return false; };
-
-            remotes.appendChild(container);
-
-            $('.videoContainer').find('video').addClass('12u');
-            $('.videoContainer').find('video').attr('controls',true)
-        }
-
-    });
-
-
-    console.log("aqui")
 });
